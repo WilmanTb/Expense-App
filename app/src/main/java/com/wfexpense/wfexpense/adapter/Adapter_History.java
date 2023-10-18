c4f92ca3bc486de92382669d0cac3c62baf44708
WilmanTb <rizki02tebe@gmail.com>
2023-10-19 01:09:03 +0700

    Update Adapter_History

--- app/src/main/java/com/wfexpense/wfexpense/adapter/Adapter_History.java
+++ app/src/main/java/com/wfexpense/wfexpense/adapter/Adapter_History.java
@@ -1,7 +1,6 @@
 package com.wfexpense.wfexpense.adapter;
 
 import android.content.Context;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,45 +10,84 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.wfexpense.wfexpense.Global_Data;
 import com.wfexpense.wfexpense.R;
 import com.wfexpense.wfexpense.model.Balance_Model;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Adapter_History extends RecyclerView.Adapter<Adapter_History.ViewHolder> {
 
-    Context context;
-    ArrayList<CharSequence> myList;
-    ArrayList<Balance_Model> incomeList;
-    ArrayList<Balance_Model> expenseList;
-    ArrayList<Balance_Model> mergeList;
+    private Context context;
+    private ArrayList<CharSequence> myList;
+    private ArrayList<Balance_Model> mergeList;
 
-    public Adapter_History(Context context, ArrayList<CharSequence> myList, ArrayList<Balance_Model> incomeList, ArrayList<Balance_Model> expenseList, ArrayList<Balance_Model> mergeList) {
+    public Adapter_History(Context context, ArrayList<CharSequence> myList, ArrayList<Balance_Model> mergeList) {
         this.context = context;
         this.myList = myList;
-        this.incomeList = incomeList;
-        this.expenseList = expenseList;
         this.mergeList = mergeList;
     }
 
     @NonNull
     @Override
-    public Adapter_History.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.list_history, parent, false);
-        return new Adapter_History.ViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adapter_History.ViewHolder holder, int position) {
-        String tanggalExpense = myList.get(position).toString();
-        holder.tanggal_expense.setText(tanggalExpense);
-        holder.rc_history.setHasFixedSize(true);
-        holder.rc_history.setLayoutManager(new LinearLayoutManager(context));
-        mergeList = new ArrayList<>();
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String currentDate = getCurrentDate();
+        holder.tanggal_expense.setText(myList.get(position).toString());
+
         Adapter_List_History adapter_list_history = new Adapter_List_History(context, mergeList);
+        holder.rc_history.setLayoutManager(new LinearLayoutManager(context));
         holder.rc_history.setAdapter(adapter_list_history);
 
+        DatabaseReference dbBalance = FirebaseDatabase.getInstance(Global_Data.dbUrl).getReference();
 
+        ValueEventListener balanceListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    mergeList.clear();
+
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        String balanceType = dataSnapshot.getKey();
+                        dbBalance.child("Balance").child(balanceType).orderByChild("tanggal").equalTo(currentDate).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+                                    for (DataSnapshot expenseSnapshot : snapshot.getChildren()) {
+                                        Balance_Model balance_model = expenseSnapshot.getValue(Balance_Model.class);
+                                        mergeList.add(balance_model);
+                                    }
+                                    adapter_list_history.notifyDataSetChanged();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        };
+
+        dbBalance.child("Balance").child("Expense").addValueEventListener(balanceListener);
+        dbBalance.child("Balance").child("Income").addValueEventListener(balanceListener);
     }
 
     @Override
@@ -61,11 +99,17 @@ public class Adapter_History extends RecyclerView.Adapter<Adapter_History.ViewHo
 
         RecyclerView rc_history;
         TextView tanggal_expense;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
             rc_history = itemView.findViewById(R.id.rc_list_expense);
             tanggal_expense = itemView.findViewById(R.id.tanggal_expense);
         }
+    }
+
+    private String getCurrentDate() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return simpleDateFormat.format(calendar.getTime());
     }
 }
